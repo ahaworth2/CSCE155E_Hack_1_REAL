634bf7cf48e1908875b78c0b7b5abae65fbee009
Abigail Elaine Haworth <ahaworth@cse.unl.edu>
2018-08-23 08:26:57 -0500

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Authors: Abigail Haworth and nick nyholm
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Abigail Haworth, Biological Systems Engineering\n");
+
+  return 0;
+}
